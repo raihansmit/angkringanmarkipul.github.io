fcae3f89149f727b9a8fa7b8cb00c0e2ecb09c7b
raihansmit <73023020+raihansmit@users.noreply.github.com>
2020-12-10 16:47:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#menu2").hide();
+    $("#btnlebih").click(function() {
+        $("#menu2").toggle("slow", function(){
+
+        });
+    });
+});
+
+function kirim() {
+    alert("Terima Kasih atas Saran Dan Masukan nya :)");
+    console.log("Terima Kasih atas Saran Dan Masukan nya :)");
+}
